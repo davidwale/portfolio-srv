dfcaccdc508b72cc03e0ec70709bef87110b3492
davidwale <davidwale2003@gmail.com>
2024-07-26 15:58:43 +0100

    Added Status Route

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sendMailRoute = require('./route/sendMail');
+const statusRoute = require('./route/status')
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/', sendMailRoute);
+app.use('/', sendMailRoute, statusRoute);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
